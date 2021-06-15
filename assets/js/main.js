0e23b8da105ac1b38eee1e5c6d4818d18da261fa
peteradeojo <peteradeojo@outlook.com>
2021-06-16 00:03:09 +0100

    final layout of page complete

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+$(() => {
+	(() => {
+		// Anonymous function to intialize tab operations
+		const tabPanes = document.querySelectorAll('.tab-pane');
+		tabPanes.forEach(function (tabpane) {
+			const tabList = tabpane.querySelector('.tabs');
+			const tabContent = tabpane.querySelector('.tab-content');
+
+			const target = tabList.querySelector('li.active') || tabList.querySelector('li');
+
+			// console.log(target);
+
+			tabContent.querySelector(target.getAttribute('data-target')).classList.add('active');
+			target.classList.add('active');
+			tabList.querySelectorAll('li').forEach((tab) => {
+				tab.addEventListener('click', showTab);
+			});
+		});
+	})();
+});
+
+function showTab() {
+	const that = this;
+	const target = this.getAttribute('data-target');
+
+	const tabpane = this.parentElement.parentElement;
+	tabpane.querySelectorAll('.tabs li').forEach((tab) => {
+		tab.classList.remove('active');
+	});
+	this.classList.add('active');
+
+	tabpane.querySelectorAll('.tab').forEach((tab) => {
+		tab.classList.remove('active');
+	});
+	tabpane.querySelector(target).classList.add('active');
+}
